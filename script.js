0f557eb8a604580150d4fb6cea37abf4c510dde2
Gustavo Bizarro <gus.bizarro@gmail.com>
2023-01-29 16:44:42 -0300

    EX 03

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function soma(a, b) {
+    return a + b
+}
+
+var num1 = parseFloat(prompt("Digite o primeiro número"));
+var num2 = parseFloat(prompt("Digite o segundo número"));
+
+var res = soma(num1, num2);
+
+document.write(" <h4> soma : </h4>" + res  );
